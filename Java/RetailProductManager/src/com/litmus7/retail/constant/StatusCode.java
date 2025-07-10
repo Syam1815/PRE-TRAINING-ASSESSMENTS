6f14469751256d48bb984601d77bb8bdc14a2c92
Syamkrishnan S <159055565+Syam1815@users.noreply.github.com>
2025-07-10 16:57:30 +0530

    Delete Java/RetailProductManager directory

--- Java/RetailProductManager/src/com/litmus7/retail/constant/StatusCode.java
+++
@@ -1,9 +0,0 @@
-package com.litmus7.retail.constant;
-
-public class StatusCode {
-public final static int SUCCESS=200;
-public final static int ERROR=201;
-public final static int INVALID_DATA=299;
-
-
-}
--- Java/RetailProductManager/src/retail.sql
+++
@@ -1,8 +0,0 @@
-use retaildb;
-CREATE TABLE product (
- productId VARCHAR(5) PRIMARY KEY,
- name VARCHAR(100) NOT NULL,
- category VARCHAR(50) NOT NULL,
- price DOUBLE NOT NULL,
- stockQuantity INT NOT NULL
-);
